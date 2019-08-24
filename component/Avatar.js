165ffb52faa4bc92d8458bd5b0f22b72f1146234
Kamel Belkheiri <kamel.belkheiri75@gmail.com>
2019-08-24 19:40:39 +0200

    change border color

--- component/Avatar.js
+++ component/Avatar.js
@@ -63,7 +63,7 @@ const styles = StyleSheet.create({
         width:100,
         height: 100,
         borderRadius : 50,
-        borderColor: 'pink',
+        borderColor: 'black',
         borderWidth : 2
 
     }
